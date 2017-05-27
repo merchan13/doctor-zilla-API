3c5105e31c6c478a23a5efe02261ca60742bc224
jmerchan <merchan1395@gmail.com>
2017-05-27 01:53:39 -0400

    Test new JSON for MedicalRecords

--- app/controllers/medical_records_controller.rb
+++ app/controllers/medical_records_controller.rb
@@ -1,5 +1,5 @@
 class MedicalRecordsController < ApplicationController
-  before_action :set_record, only: [:show, :update]
+  before_action :set_record, only: [:show, :update, :physic_data, :backgrounds]
 
   # GET /medical_records
   def index
@@ -9,7 +9,29 @@ class MedicalRecordsController < ApplicationController
 
   # GET /medical_records/:id
   def show
-    json_response(@record)
+    json = {  :address => @records.address,
+              :background => @record.backgrounds,
+              :birthday => @record.birthday,
+              :cellphone_number: => @record.cellphone_number,
+              :created_at => @record.created_at,
+              :document => @record.document,
+              :document_type => @record.document_type,
+              :email => @record.email,
+              :first_consultation_date => @record.first_consultation_date,
+              :gender => @record.gender,
+              :id => @record.id,
+              :insurance => @record.insurance.name,
+              :last_name => @record.last_name,
+              :name => @record.name,
+              :occupation_id => @record.occupation.name,
+              :phone_number => @record.phone_number,
+              :physic_data => @record.physic_data
+              :profile_picture => @record.profile_picture.url,
+              :referred_by => @record.referred_by,
+              :representative_document => @record.representative_document,
+              :updated_at => @record.updated_at
+            }
+    json_response(json)
   end
 
   # PUT /medical_records/:id
@@ -22,6 +44,16 @@ class MedicalRecordsController < ApplicationController
   def search
     @records = current_user.medical_records.search(params[:search_param])
     json_response(@records)
+  end
+
+  # GET /medical_records/physic_data/:id
+  def physic_data
+    json_response(@record.physic_data)
+  end
+
+  # GET /medical_records/backgrounds/:id
+  def physic_data
+    json_response(@record.backgrounds)
   end
 
   private
