0288f69b6a8ccd33ee6d7e7e09ee84e4918f9909
jmerchan <merchan1395@gmail.com>
2017-09-10 22:36:27 -0400

    Fixed storage route AWS

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -1,20 +1,16 @@
 # encoding: utf-8
 class AttachmentUploader < CarrierWave::Uploader::Base
-
   include CarrierWave::MiniMagick
-
-  # Choose what kind of storage to use for this uploader:
-  #storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.medical_record.last_name.gsub(/ /, "")}_#{model.medical_record.id}/#{model.class.to_s.underscore}s/#{model.id}"
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
-    %w(pdf doc htm html docx)
+    %w(pdf doc htm html docx jpg jpeg gif png)
   end
 end
